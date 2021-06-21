f8b1947b9839e9420365f4499437ea54536c0d60
Loukis-13 <ze.zinho-2010@hotmail.com>
2021-06-21 18:49:11 -0300

    first

---
+++ README.md
@@ -0,0 +1,19 @@
+## File Metadata Microservice
+
+[Free Code Camp](https://www.freecodecamp.org/) API project made with Node.js that:
+
+* Shows the actual date
+* Receives a date passed in epochs or string and returns it in epochs and formated to UTC string
+
+### End-points
+
+```
+/api
+```
+GET returns actual date
+
+
+```
+/api/{date}
+```
+GET returns the conversion of date passed in epochs or string and returns it in epochs and formated to UTC string
---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  if ( req.params['date'].match(/^\d+$/) ) {
+    time = new Date;
+    time.setTime(parseInt(req.params['date']));
+  }
+  else if ( req.params['date'].match(/\d{4}/) ) {
+    time = new Date(req.params['date']);
+  }
+  else {
+    res.json({error: "Invalid Date"});
+  }
+
+  res.json({unix: time.getTime(), utc: time.toUTCString()});
+});
+
+app.get("/api/", function (req, res) {
+  time = new Date;
+  res.json({unix: time.getTime(), utc: time.toUTCString()});
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
